f5381841dbcd3207fc5598c26478ead5551d2938
electrosaurus <n.y.govorukhin@gmail.com>
2021-07-19 19:31:38 +0300

    Add requirements

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,8 @@ pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
 
+import sphinx_rtd_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
